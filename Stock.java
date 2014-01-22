d165e3e6bb907b3b40df94c8452d9fd9a8148c28
Fjoren <braden@asobu.org>
2014-01-21 22:51:36 -0600

    Finished 8.2

--- Stock.java
+++ Stock.java
@@ -1,4 +1,15 @@
 class Stock {
-	Stock () {
+	String symbol;
+	String name;
+	double previousClosingPrice;
+	double currentPrice;
+	
+	Stock (String symbol, String name) {
+		this.symbol = symbol;
+		this.name = name;
+	}
+	
+	double getChangePercent () {
+		return (currentPrice-previousClosingPrice)/previousClosingPrice;
 	}
 }
--- StockTests.java
+++ StockTests.java
@@ -1,5 +1,8 @@
 class StockTests {
 	public static void main(String[] args) {
-		Stock test = new Stock;
+		Stock test = new Stock("ORCL", "Oracle Corporation");
+		test.previousClosingPrice = 34.5;
+		test.currentPrice = 34.35;
+		System.out.println(test.getChangePercent());
 	}
 }
